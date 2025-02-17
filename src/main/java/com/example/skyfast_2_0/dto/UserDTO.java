a6b57d25dc4e09461461efef72ee7982847f6fa6
nguyenluong00 <luong.nguyengiahien@vti.com.vn>
2025-02-17 21:34:23 +0700

    update user

--- src/main/java/com/example/skyfast_2_0/dto/UserDTO.java
+++ src/main/java/com/example/skyfast_2_0/dto/UserDTO.java
@@ -1,8 +1,13 @@
 package com.example.skyfast_2_0.dto;
 
 import com.example.skyfast_2_0.constant.Role;
-import lombok.*;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -10,16 +15,17 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Setter
 @Getter
-
 public class UserDTO {
     private Integer id;
     private String userName;
     private String email;
     private String password;
     private String fullName;
-    private Integer phoneNumber;
+    private String phoneNumber;
     private String address;
     private Role role;
+    private LocalDate dateOfBirth;
+    private String status;
     private LocalDateTime createdAt;
     private LocalDateTime updateAt;
 }
