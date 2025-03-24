d9dc7f51670498f34fc4329cfd09e76cef0b9835
dthang29 <thangbdhe180731@fpt.edu.vn>
2025-03-24 15:25:16 +0700

    Add Authorization, VNPay Payment, QR Code Payment

--- src/main/java/com/example/skyfast_2_0/controller/AirPlaneController.java
+++ src/main/java/com/example/skyfast_2_0/controller/AirPlaneController.java
@@ -1,8 +1,12 @@
 package com.example.skyfast_2_0.controller;
 
 import com.example.skyfast_2_0.entity.Airplane;
+import com.example.skyfast_2_0.entity.User;
+import com.example.skyfast_2_0.repository.T_UserRepository;
 import com.example.skyfast_2_0.service.AirPlaneService;
+import com.example.skyfast_2_0.service.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,16 +20,30 @@ import java.util.List;
 public class AirPlaneController {
     @Autowired
     private AirPlaneService airPlaneService;
+    @Autowired
+    private UserProfileService userProfileService;
+    @Autowired
+    private T_UserRepository t_userRepository;
 
     @GetMapping
-    public String getAllAirplanes(Model model) {
+    public String getAllAirplanes(Model model, Authentication authentication) {
+        if(authentication != null){
+            String email = userProfileService.getUserEmail(authentication);
+            User user = t_userRepository.findByEmail(email);
+            model.addAttribute("username", user.getUserName());
+        }
         List<Airplane> airplanes = airPlaneService.getAllAirplanes();
         model.addAttribute("airplanes", airplanes);
-        return "AirPlane";
+        return "Airplane";
     }
 
     @GetMapping("/{id}")
-    public String getAirplaneById(@PathVariable("id") int id, Model model) {
+    public String getAirplaneById(@PathVariable("id") int id, Model model, Authentication authentication) {
+        if(authentication != null){
+            String email = userProfileService.getUserEmail(authentication);
+            User user = t_userRepository.findByEmail(email);
+            model.addAttribute("username", user.getUserName());
+        }
         Airplane airplane = airPlaneService.getAirplaneById(id);
         if (airplane == null) {
             throw new RuntimeException("Airplane not found with id: " + id);
---
+++ src/main/java/com/example/skyfast_2_0/repository/T_PaymentRepository.java
@@ -0,0 +1,8 @@
+package com.example.skyfast_2_0.repository;
+
+import com.example.skyfast_2_0.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface T_PaymentRepository extends JpaRepository<Payment, Integer> {
+
+}
